56292fbb9fe8e6143e7dcb30713c74d9703140d0
Mohammad Saqib <saqib96181@gmail.com>
2025-03-05 02:13:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(){
+    let a=parseInt(num1.value);
+    let b=parseInt(num2.value);
+    let result=a+b;
+    alert(result);
+  }
+  function sub(){
+    let a=parseInt(num1.value);
+    let b=parseInt(num2.value);
+    let result=a-b;
+    alert(result);
+  }
+  function mul(){
+    let a=parseInt(num1.value);
+    let b=parseInt(num2.value);
+    let result=a*b;
+    alert(result);
+  }
+  function div(){
+    let a=parseInt(num1.value);
+    let b=parseInt(num2.value);
+    let result=a/b;
+    alert(result);
+  }
